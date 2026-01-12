6e289f47b96462bad92223f86d74c69ff389ae24
hasan <hasan.shobri@outlook.com>
2026-01-12 21:56:04 +0700

    Simplify `WeatherForecastController` by using constructor parameter directly.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,18 +6,11 @@ namespace hello_world_from_id.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(IWeatherForecastService service) : ControllerBase
 {
-    private readonly IWeatherForecastService _service;
-
-    public WeatherForecastController(IWeatherForecastService service)
-    {
-        _service = service;
-    }
-
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return _service.GetForecast();
+        return service.GetForecast();
     }
 }
